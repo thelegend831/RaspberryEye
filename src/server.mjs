66296f5a7b6c65c675f24688e7f53696b09b2c49
Rob <bfe5e925@opayq.com>
2019-02-04 22:21:29 +0000

    - adding more test code

--- src/server.mjs
+++ src/server.mjs
@@ -1,8 +1,27 @@
 import admin from 'firebase-admin';
+import serviceAccount from './key.json';
+import fs from 'fs';
+import path from 'path';
 
-var serviceAccount = require("private/raspberryeye-1804e-firebase-adminsdk-0v02x-b0f7994bbd.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://raspberryeye-1804e.firebaseio.com"
   });
+
+
+const bucket = admin.storage().bucket();
+//var ref = admin.storage().ref();
+
+// var task = ref.put(file);
+
+// task.on('state_change', (snapshot) => {
+//   var percentage = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+//   console.log(percentage);
+// }, (error) => {
+//   console.error(error);
+// }, () => {
+//   console.log('Upload finished');
+// });
+
+//https://www.youtube.com/watch?v=Z87OZtIYC_0&list=PLnepGR_yu2ekWVHK_PEXGBv3TxFDOrSLs&index=5
