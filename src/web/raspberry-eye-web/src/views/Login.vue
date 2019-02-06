e67781846443d2671e4d2e559cce77f6a236995d
Rob Davis <robert.lyndon.davis@gmail.com>
2019-02-06 15:00:07 +0000

    - vue router stuff

---
+++ src/web/raspberry-eye-web/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button @click="login">Connection</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {}
+    },
+    methods: {
+        login: function() {
+            this.$router.replace('home');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
