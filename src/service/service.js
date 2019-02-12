82b3ce9ba81735bf8c25875fa2246e6d45aad0eb
Rob <bfe5e925@opayq.com>
2019-02-12 22:08:56 +0000

    - debounced recording

--- src/service/service.js
+++ src/service/service.js
@@ -6,82 +6,97 @@ var fs = require('fs');
 var admin = require('firebase-admin');
 var serviceAccount = require('./key.json');
 
+// Setup the debounce flag
+var monitoring = true;
+
+// Admin firebase
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://raspberryeye-1804e.firebaseio.com"
+});
+
+console.log('Admin init done');
+
 // Setup the motion detection to work on GPIO pin 17
 var sensor = new Gpio(17, 'in', 'both');
 
-sensor.watch(function (err, value) {
-   if (err) { 
-     console.error('There was an error', err);
-   return;
-   }
-  
-   if (value) {
-       handleMotionDetection()
-   }
-});
+console.log('Gpio init done');
 
-function handleMotionDetection() {
-    var fileName = captureImage(uploadImage);
-}
+sensor.watch(function (err, value) {
+    if (err) {
+        console.error('There was an error', err);
+        return;
+    }
+
+    if (value && monitoring) {
+        
+        // Toggle the flag off 
+        monitoring = false;
+
+        // start the debounce timer
+        setTimeout(function() {
+            monitoring = true;
+        }, config.process.debounce);
+
+        captureImage(uploadImage);
+    }
+});
 
 function captureImage(callback) {
     var fileName = Date.now() + config.ffmpeg.framesExt;
-    var cmd = 'ffmpeg -y -i rtsp://' 
-            + config.camera.username 
-            + ':' 
-            + config.camera.password 
-            + '@' 
-            + config.camera.ip 
-            + '/Streaming/channels/' 
-            + config.camera.channel 
-            + ' -vframes ' 
-            + config.ffmpeg.framesCount + ' ' 
-            + path.join(path.resolve(), config.ffmpeg.framesDirectory) 
-            + '/' 
-            + fileName;
-    
+    var cmd = 'ffmpeg -y -i rtsp://' +
+        config.camera.username +
+        ':' +
+        config.camera.password +
+        '@' +
+        config.camera.ip +
+        '/Streaming/channels/' +
+        config.camera.channel +
+        ' -vframes ' +
+        config.ffmpeg.framesCount + ' ' +
+        path.join(path.resolve(), config.ffmpeg.framesDirectory) +
+        '/' +
+        fileName;
+
     console.log('Executing command: ' + cmd);
 
-    exec(cmd, function(err, stdout, stderr) {
+    exec(cmd, function (err, stdout, stderr) {
         if (err) {
             console.error(err);
             return;
         }
 
         console.log(fileName + ' Captured');
-	
-	callback(fileName);
+
+        callback(fileName);
     });
 }
 
 function uploadImage(fileName) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-	databaseURL: "https://raspberryeye-1804e.firebaseio.com"   
-    });
-
-    console.log('Admin init done');
-
+    
     const db = admin.firestore();
     var fileContents = base64_encode('./frames/' + fileName);
-    
-    console.log('File Contents collected');
-    //var timeStamp = new Date().toISOString();
+
+    console.log('File Contents converted to base64');
+
     db.collection('events').doc(fileName).set({
         date: new Date(),
-	image: fileContents,
-	sensor: 'front'
+        image: fileContents,
+        sensor: 'front'
     });
 
     console.log('Record has been added to the collection.');
+}
+
+function base64_encode(fileName) {
+    return fs.readFileSync(fileName, {
+        encoding: 'base64'
+    });
 }
 
 function unexportOnClose() {
     sensor.unexport();
 };
 
-function base64_encode(fileName) {
-    return fs.readFileSync(fileName, { encoding: 'base64' });
-}
-  
+// Clean up on exit
 process.on('SIGINT', unexportOnClose);
