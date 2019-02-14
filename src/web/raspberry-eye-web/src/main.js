45a1368037cc82e99c94a2cac18572b49d4580ea
Rob Davis <robert.lyndon.davis@gmail.com>
2019-02-14 17:38:23 +0000

    - added sass file for bulma overrides

--- src/web/raspberry-eye-web/src/main.js
+++ src/web/raspberry-eye-web/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import firebase from 'firebase'
-import 'bulma/css/bulma.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+require('./assets/app.scss');
 
 library.add(faEnvelope);
 library.add(faLock);
