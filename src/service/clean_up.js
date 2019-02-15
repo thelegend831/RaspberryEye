5af5ad2f56c4c9d4a6bf6c8ff2ed25f3f8b42d41
Rob Davis <bfe5e925@opayq.com>
2019-02-15 17:03:01 +0000

    - added clean up script

--- src/service/clean_up.js
+++ src/service/clean_up.js
@@ -6,16 +6,26 @@ admin.initializeApp({
     databaseURL: "https://raspberryeye-1804e.firebaseio.com"
 });
 
+// the number of hours back to keep
+const hours = 4;
+
 const db = admin.firestore();
+let date = new Date();
+date.setTime(date.getTime() - (hours * 60 * 60 * 1000));
+var count = 0;
+console.log(date.toISOString());
 
-const date = new Date() + 1;
-
-db.collection('events').get()
+db.collection('events').orderBy('date', 'desc').startAfter(date).get()
     .then(function(snapshot) {
+        var batch = db.batch();
         snapshot.forEach(function(doc) {
-            console.log(doc.data().Date);
+            batch.delete(doc.ref);
+            console.log('Deleting: ' + doc.data().date.toDate());
+            count++;
         });
+
+        return batch.commit();
     })
     .then(function() {
-        console.log('Clean up done');
+        console.log('Clean up of ' + count + ' records done');
     });
