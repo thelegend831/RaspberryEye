3344d8c2022705eeb8f425b1264a3825456f1b0e
Rob <bfe5e925@opayq.com>
2019-02-19 21:03:12 +0000

    - added ui for new settings

--- src/web/raspberry-eye-web/src/views/Settings.vue
+++ src/web/raspberry-eye-web/src/views/Settings.vue
@@ -1,50 +1,66 @@
 <template>
-    <div class="section">
-        <!-- <div class="field">
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <div class="column is-4 is-offset-4">
+          <div class="field">
             <div class="control">
-                <input id="notificationsSwitch" v-model="notifications" type="checkbox" name="notificationsSwitch" 
-                    class="switch input" @click="handleNotificationsToggle">
-                <label for="notificationsSwitch">Notifications</label>
+              <input
+                id="notificationsSwitch"
+                v-model="notifications"
+                v-on:change="handleNotificationsToggle"
+                type="checkbox"
+                name="notificationsSwitch"
+                class="switch input"
+              >
+              <label for="notificationsSwitch">Notifications</label>
             </div>
-        </div> -->
-        <div class="field">
+          </div>
+          <div class="field">
             <div class="control">
-                    <slider id="groupTime" type="success" size="large" :value="groupTime" :max="600" :step="10" is-fullwidth @change="handleGroupTimeChange"></slider>
-                <label for="groupTime">Group Time</label>
+              <label for="groupTime">Group Time</label>
+              <input
+                name="groupTime"
+                id="groupTime"
+                type="text"
+                v-model="groupTime"
+                v-on:change="handleGroupTimeChange"
+                class="input"
+              >
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </section>
 </template>
 
 <script>
-import Slider from 'vue-bulma-slider'
-
 export default {
-    name: 'Settings',
-    props: {
-        settings: Object
+  name: "Settings",
+  props: {
+    settings: Object
+  },
+
+  data() {
+    return {
+      notifications: true,
+      groupTime: 0
+    };
+  },
+
+  methods: {
+    handleGroupTimeChange(e) {
+      console.log(this.groupTime);
     },
 
-    data() {
-        return {
-            notifications: false,
-            groupTime: 0
-        }
-    },
-
-    methods: {
-        handleGroupTimeChange(e) {
-            console.log(this.groupTime);
-        },
-
-        handleNotificationsToggle(e) {
-            // TODO: save this user settings to the firebase store
-            console.log(this.notifications)
-        }
+    handleNotificationsToggle(e) {
+      // TODO: save this user settings to the firebase store
+      console.log(this.notifications);
     }
-}
+  }
+};
 </script>
 
 <style>
-
 </style>
